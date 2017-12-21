1cf58baef7f8a24e3c4a1570df2542a51d870260
Jay Flowers <jay.flowers@gmail.com>
2017-12-21 14:15:04 -0500

    commit for version : 1.0.1

--- recipes/base.rb
+++ recipes/base.rb
@@ -31,28 +31,6 @@ include_recipe 'jenkins_ecosystem::sudo'
 include_recipe 'jenkins_ecosystem::aws'
 include_recipe 'jenkins_ecosystem::esxi'
 
-ruby_version = '2.2.4'
-ruby_binary_path = "#{node[:jenkins_ecosystem][:home]}/.rbenv/versions/#{ruby_version}/bin/ruby"
-
-bash 'install_ruby' do
-  user 'jenkins'
-  group 'jenkins'
-  cwd node[:jenkins_ecosystem][:home]
-  code %^
-sudo su - jenkins <<-'ENDCOMMANDS'
-  eval "$(rbenv init - )"
-
-  rbenv install "#{ruby_version}"
-ENDCOMMANDS
-^
-  creates ruby_binary_path
-end
-
-link '/usr/local/bin/ruby' do
-  to ruby_binary_path
-  link_type :symbolic
-end
-
 link '/bin/bsdtar' do
   to '/bin/tar'
   link_type :symbolic
